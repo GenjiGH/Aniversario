38e677dfbeab664fd7806249c937610e375d8c79
GenjiGH <geoxgenji@gmail.com>
2025-05-24 14:21:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-window.onload = function () {
+window.onload = function() {
   setTimeout(() => {
     const intro = document.getElementById('intro');
     const cards = document.getElementById('cards-container');
 
+    intro.style.transition = 'opacity 1s ease-out';
     intro.style.opacity = 0;
 
     setTimeout(() => {
       intro.style.display = 'none';
       cards.classList.remove('hidden');
-      cards.classList.add('visible');
-    }, 1000); // Esperar 1s más por la transición
-  }, 5000); // Mostrar intro durante 5 segundos
+    }, 1000); // Tiempo para desaparecer
+  }, 5000); // Espera inicial
 };
 
-function flipCard(cardElement) {
-  if (!cardElement.classList.contains("flipped")) {
-    cardElement.classList.add("flipped");
+function flipCard(cardElement, index) {
+  if (!cardElement.classList.contains('flipped')) {
+    cardElement.classList.add('flipped');
   }
 }
