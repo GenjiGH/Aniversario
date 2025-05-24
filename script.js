643d8ce9272697bcd4156081197e41df08a7a444
GenjiGH <geoxgenji@gmail.com>
2025-05-24 12:30:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ window.onload = function () {
   setTimeout(() => {
     const intro = document.getElementById('intro');
     const cards = document.getElementById('cards-container');
+
     intro.style.opacity = 0;
 
     setTimeout(() => {
       intro.style.display = 'none';
       cards.classList.remove('hidden');
-    }, 1000); // Tiempo para transición
-  }, 5000); // Esperar 5 segundos antes de desaparecer
+      cards.classList.add('visible');
+    }, 1000); // Esperar 1s más por la transición
+  }, 5000); // Mostrar intro durante 5 segundos
 };
 
-function flipCard(cardElement, index) {
+function flipCard(cardElement) {
   if (!cardElement.classList.contains("flipped")) {
     cardElement.classList.add("flipped");
   }
